7612746e46f446a173b01662a2ae9e3e1ef8804d
Akash Patil <akashpatil2002@gmail.com>
2025-06-13 21:39:44 -0400

    Successfully implemented reverse string using stack

--- 19-Reverse-String-Stack.py
+++ 19-Reverse-String-Stack.py
@@ -1,3 +1,24 @@
 #Now we shall solve Reverse String using stack 
 
 str = ["h", "e", "l", "l", "o"]
+
+#in python, stacks are implemented using python lists
+stack1 = []
+l = 0
+
+for ele in str:
+    stack1.append(ele)
+
+while stack1:
+    str[l] = stack1.pop()
+    l += 1
+
+print(str)
+
+
+
+
+
+
+
+
