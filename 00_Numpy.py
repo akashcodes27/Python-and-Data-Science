0bf96ec3ec4fd8efde679a77852069fc78d390b9
Akash Patil <akashpatil2002@gmail.com>
2025-06-28 21:23:47 -0400

    DataFrames and Series

--- 00_Numpy.py
+++ 00_Numpy.py
@@ -6,12 +6,24 @@ We Gonna Learn Some pandas
 In Pandas, for dealing with structured datasets,  We shall be working with 2 kinds of data structures: 
 1. One-Dimensional Data Structure: Series  (Appears like a single column in output screen)
 2. Two-Dimensional Data Structure: DataFrame (Appears like a table od rows and columns in output screen)
+
+
+Difference between Series and DataFrames. Series takes Python's Lists as input. DataFrames take Python's Dictionaries as input
 """
 
 import pandas as pd 
 
-s1 = pd.Series(["BMW", "Honda", "Audi"])
+s1 = pd.Series(["BMW", "Honda", "Audi", "Mazda", "Lexus"])
 
-print(s1)
+s2 = pd.Series(["Red", "White", "Brown", "Grey", "Black"])
+
+s3 = pd.Series(["2020", "2018", "2024", "2017", "2019"])
+
+
+"While Constructing dataframes we can simply create individual Series and combine them inside DataFrame dictionary"
+
+df1 = pd.DataFrame({ "Brand":s1, "Color":s2, "Year":s3})
+
+print(df1)
 
 
