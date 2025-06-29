a4f123047baa3b41c76123559ae200da76a23f86
Akash Patil <akashpatil2002@gmail.com>
2025-06-28 21:12:58 -0400

    Pandas Notes

--- 00_Numpy.py
+++ 00_Numpy.py
@@ -1,5 +1,11 @@
 """
 We Gonna Learn Some pandas
+
+
+
+In Pandas, for dealing with structured datasets,  We shall be working with 2 kinds of data structures: 
+1. One-Dimensional Data Structure: Series  (Appears like a single column in output screen)
+2. Two-Dimensional Data Structure: DataFrame (Appears like a table od rows and columns in output screen)
 """
 
 import pandas as pd 
