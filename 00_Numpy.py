70a1a1e44044f41a14ce4ddc108327bad3820578
Akash Patil <akashpatil2002@gmail.com>
2025-07-11 18:13:15 -0400

    Understanding slicing example: [:, 1:]

--- 00_Numpy.py
+++ 00_Numpy.py
@@ -309,8 +309,7 @@ print(sales_data[3])
 print(sales_data[1,3])  # 1 navigates rows, 3 navigates columns of that specific row and final result is a single value
 print(sales_data[:, 3])   # considers all rows, returns a column of values(considering all rows)
 print(sales_data[3 , :])   #Same as sales_data[3], navigates to row with index 3, and considers all columns for that specific row
-print(sales_data[:, 1:])  #Like how 1: prints rows from 1 idx to end.  :, 1: prints columns from idx 1 to end
-# Difference? Just flip rows to columns 
+print(sales_data[:, 1:])  #-> : considers all rows, 1: considers columns from index 1
 
 # Lets now calculate Total revenue for each year. We shall do this by doing: using sum() function and performing Column Slicing
 result_arr = np.sum(sales_data[:, 1:], axis=0)  #This gets id of id column
