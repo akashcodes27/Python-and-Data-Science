8a17ce2a9c8460bcaa7be24ed88c1fc5f8c0869f
Akash Patil <akashpatil2002@gmail.com>
2025-07-12 13:10:16 -0400

    Scikit Learn - ACME Insurance automated system for predicting insurance

---
+++ 00-scikit_learn.py
@@ -0,0 +1,10 @@
+'''
+ACME insurance is an insurance company that offers affordable health insurance to thousands of customers. Lets assume that I am the lead data scientist at ACME insurance. I have been tasked with building an automated system(basically a trained ML-model end-to-end pipeline) which is able to estimate the medical expenditure for the company's new customers. In order to make such an estimation, I am supposed to take following features into consideration such as: age, sex, BMI, smoking habits, children, region of residence.
+Estimate that our system generates for each customer will enable us to determine how much insurance to charge from every customer
+'''
+
+
+
+
+
+
